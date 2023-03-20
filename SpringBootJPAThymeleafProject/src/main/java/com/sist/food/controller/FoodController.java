a452f243dbd467c2410332a268797930a85c1fbe
kyunglyn <amy3249@naver.com>
2023-03-20 17:50:36 +0900

    230320_thymeleaf_include

---
+++ SpringBootJPAThymeleafProject/src/main/java/com/sist/food/controller/FoodController.java
@@ -0,0 +1,31 @@
+package com.sist.food.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+import com.sist.food.dao.*;
+import com.sist.food.entity.*;
+@Controller
+public class FoodController {
+	@Autowired
+	private CategoryDAO dao;
+	@GetMapping("/main")
+	public String food_main(Model model)
+	{
+		List<CategoryEntity> list=dao.categoryListData();
+		model.addAttribute("list",list);
+		model.addAttribute("main_content","main/home"); //main/home이 include
+		return "main";
+	}
+	
+	@GetMapping("/food/category")
+	public String food_category(Model model)
+	{
+		List<CategoryEntity> list=dao.categoryListData();
+		model.addAttribute("list",list);
+		return "food/category";
+	}
+}
---
+++ SpringBootJPAThymeleafProject/src/main/java/com/sist/food/dao/CategoryDAO.java
@@ -0,0 +1,11 @@
+package com.sist.food.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import com.sist.food.entity.*;
+@Repository
+public interface CategoryDAO extends JpaRepository<CategoryEntity,Integer>{
+	@Query(value="SELECT cno,title,subject,poster,link FROM project_category",nativeQuery = true)
+	public List<CategoryEntity> categoryListData();
+}
